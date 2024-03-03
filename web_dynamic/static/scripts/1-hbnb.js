56e41a11ef250b110385f1e28cad38fbc48620e4
oussama-el1 <elhadraoui284@gmail.com>
2024-03-03 15:37:03 +0100

    task 3

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var selectedAmenities = {};
+
+    $('.amenity-checkbox').change(function () {
+        var amenityId = $(this).data('id');
+        var amenityName = $(this).data('name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        let valuesArray = [];
+        $.each(selectedAmenities, function (key, value) {
+            valuesArray.push(value);
+        });
+
+        console.log(valuesArray);
+        $('#listamenity').text("selected : " + valuesArray.join(', '));
+    });
+});
